4f593fa0e201aec19e75562fbd67606089963e3d
Jitka H:)bnerová <jitka.darbujanova@gmail.com>
2023-05-04 08:37:22 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import KeyCloakService from './security/KeycloakService';
+
+function logout(){
+  KeyCloakService.CallLogout();
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>Welcome {KeyCloakService.GetUserName()} </p>
+        <p>Roles: {KeyCloakService.GetUserRoles()?.join(" ")}</p>
+        <button onClick={
+          () => { 
+            console.log("button clicked 2"); 
+            logout();} 
+          }>Log Out</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
